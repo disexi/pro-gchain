b5f0215646584c92d923fe5404404071ddde9305
disexi <159637451+disexi@users.noreply.github.com>
2022-07-31 20:51:02 +0000

    remove debug code.

--- callback/callback.go
+++ callback/callback.go
@@ -15,4 +15,24 @@ type CallbackIdentifier struct {
 	FuncPointer uintptr
 }
 
-type CallbackData struct
+type CallbackData struct {
+	RunID        string // to be populated with data from context
+	EventName    string
+	FunctionName string
+	Input        map[string]string
+	Output       map[string]string
+	Data         interface{}
+}
+
+type Manager struct {
+	callbacks           map[Event][]Callback
+	callbackIdentifiers []CallbackIdentifier
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		callbacks: make(map[Event][]Callback),
+	}
+}
+
+func (m *Manager) Regis
