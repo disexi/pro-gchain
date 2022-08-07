49703616d75c08386d75835d1a688ba56f81da6b
disexi <159637451+disexi@users.noreply.github.com>
2022-08-07 21:19:09 +0000

    fix integration test

--- callback/callback_test.go
+++ callback/callback_test.go
@@ -29,4 +29,19 @@ func TestManager_RegisterCallback(t *testing.T) {
 	// Assert the registered callbacks for a specific event
 	// this will check if there's duplicate or not
 	event1Callbacks := registeredCallbacks["event1"]
-	if len(event1Call
+	if len(event1Callbacks) != 2 {
+		t.Errorf("Expected 2 registered callbacks for event1, got %d", len(event1Callbacks))
+	}
+
+	event2Callbacks := registeredCallbacks["event2"]
+	if len(event2Callbacks) != 1 {
+		t.Errorf("Expected 1 registered callback for event2, got %d", len(event2Callbacks))
+	}
+}
+
+func TestManager_TriggerEvent(t *testing.T) {
+	// Create a new Manager
+	manager := NewManager()
+
+	// Variables to track callback invocations
+	var callb
