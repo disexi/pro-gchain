2ea6e524b6ff8a1c41298924515fbce77fa0f5a6
disexi <159637451+disexi@users.noreply.github.com>
2022-08-07 17:58:50 +0000

    v1.9.0

--- callback/callback_test.go
+++ callback/callback_test.go
@@ -16,4 +16,17 @@ func TestManager_RegisterCallback(t *testing.T) {
 	manager.RegisterCallback("event1", callback3)
 
 	// Register duplicate callback for event1
-	manager.Register
+	manager.RegisterCallback("event1", callback1)
+
+	// Inspect the registered callbacks
+	registeredCallbacks := manager.inspect()
+
+	// Assert the number of registered callbacks
+	if len(registeredCallbacks) != 2 {
+		t.Errorf("Expected 2 registered events, got %d", len(registeredCallbacks))
+	}
+
+	// Assert the registered callbacks for a specific event
+	// this will check if there's duplicate or not
+	event1Callbacks := registeredCallbacks["event1"]
+	if len(event1Call
