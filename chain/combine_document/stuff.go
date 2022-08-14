cabb5b73450f11b264832d3800eb4fd86eb3953a
disexi <159637451+disexi@users.noreply.github.com>
2022-08-14 22:28:09 +0000

    Modify setup.py

--- chain/combine_document/stuff.go
+++ chain/combine_document/stuff.go
@@ -6,3 +6,18 @@ import (
 
 	"github.com/wejick/gchain/chain"
 	"github.com/wejick/gchain/chain/llm_chain"
+	"github.com/wejick/gchain/model"
+	"github.com/wejick/gchain/prompt"
+)
+
+var _ CombinedDocument = &StuffCombineDocument{}
+var _ chain.BaseChain = &StuffCombineDocument{}
+
+// StuffCombineDocument chain to feed text document to LLM with specified prompt
+type StuffCombineDocument struct {
+	prompt            *prompt.PromptTemplate
+	llmChain          *llm_chain.LLMChain
+	promptTemplateKey string
+}
+
+// NewStuffCombineDocument creates new instance of StuffCombin
