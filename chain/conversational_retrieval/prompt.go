5589d6bb7e239f40b881d2f1e378bccf9829245e
disexi <159637451+disexi@users.noreply.github.com>
2022-09-11 17:10:43 +0000

    some text

--- chain/conversational_retrieval/prompt.go
+++ chain/conversational_retrieval/prompt.go
@@ -2,4 +2,19 @@ package conversational_retrieval
 
 // FIXME : we need a way to make KB lookup more generic
 var instruction = `INSTRUCTION
-You have access t
+You have access to this knowledge base (KB) :
+name | description
+wikipedia | knowledge base to find any general information, I accept standalone keywords as query.
+
+CONVERSATION
+{{.history}}
+User: {{.question}}
+
+When responding to me, please output a response in one of two formats:
+
+**Option 1:**
+use this if you can answer directly without KB lookup
+Answer in this following json schema:
+
+{
+"conversation_context":"longer additional context to understand user 
