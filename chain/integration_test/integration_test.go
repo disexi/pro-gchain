d475b8b2cbd04e76c506d359574b48b44c9a9ad6
disexi <159637451+disexi@users.noreply.github.com>
2022-09-20 13:48:35 +0000

    fixing naming

--- chain/integration_test/integration_test.go
+++ chain/integration_test/integration_test.go
@@ -15,4 +15,19 @@ import (
 	"github.com/wejick/gchain/chain/conversational_retrieval"
 	"github.com/wejick/gchain/chain/llm_chain"
 	"github.com/wejick/gchain/chain/summarization"
-	wikipedia "gi
+	wikipedia "github.com/wejick/gchain/datastore/wikipedia_retriever"
+	"github.com/wejick/gchain/eval"
+	"github.com/wejick/gchain/model"
+	_openai "github.com/wejick/gchain/model/openAI"
+	"github.com/wejick/gchain/prompt"
+	"github.com/wejick/gchain/textsplitter"
+)
+
+var llmModel model.LLMModel
+var chatModel model.ChatModel
+
+func TestMain(m *testing.M) {
+	fmt.Println("Running integration tests...")
+	// Perform any setup or initialization here
+
+	var authToken = os.Geten
