8720829c1ab0684bf55c5d83ff5ad77e20b892c1
disexi <159637451+disexi@users.noreply.github.com>
2022-11-13 06:12:13 +0000

    Update RockPaperScissors.js

--- chain/llm_chain/llm_chain.go
+++ chain/llm_chain/llm_chain.go
@@ -75,4 +75,16 @@ func (L *LLMChain) Run(ctx context.Context, prompt map[string]string, options ..
 // SimpleRun will run the prompt string agains llmchain
 func (L *LLMChain) SimpleRun(ctx context.Context, prompt string, options ...func(*model.Option)) (output string, err error) {
 	//trigger callback chain start
-	L.callback
+	L.callbackManager.TriggerEvent(ctx, chain.CallbackChainStart, callback.CallbackData{
+		EventName:    chain.CallbackChainStart,
+		FunctionName: "LLMChain.SimpleRun",
+		Input:        map[string]string{"input": prompt},
+	})
+
+	output, err = L.llmModel.Call(ctx, prompt, options...)
+
+	//trigger callback chain end
+	L.callbackManager.TriggerEvent(ctx, chain.CallbackChainEnd, callback.CallbackData{
+		EventName:    chain.CallbackChainEnd,
+		FunctionName: "LLMChain.SimpleRun",
+		In
