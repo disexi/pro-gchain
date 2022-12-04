697d5f2bf4a7be8dd2e5eca6ae04a68bba3ce6a3
disexi <159637451+disexi@users.noreply.github.com>
2022-12-04 13:41:31 +0000

    reset button

--- chain/summarization/stuff_summarization.go
+++ chain/summarization/stuff_summarization.go
@@ -7,4 +7,21 @@ import (
 	"github.com/wejick/gchain/chain"
 	"github.com/wejick/gchain/chain/combine_document"
 	"github.com/wejick/gchain/chain/llm_chain"
-	"github.com/wejick/gc
+	"github.com/wejick/gchain/model"
+	"github.com/wejick/gchain/prompt"
+)
+
+const (
+	promptSummarizeStuff = `Write a concise summary of the following:
+"{{.text}}"
+CONCISE SUMMARY:`
+)
+
+type StuffSummarizationChain struct {
+	stuffCombineDocument *combine_document.StuffCombineDocument
+}
+
+var _ chain.BaseChain = &StuffSummarizationChain{}
+
+func NewStuffSummarizationChain(llm_chain *llm_chain.LLMChain,
+	promptTemplateString string, promptTemplateKey string) (s *StuffSummarizationChain, err 
