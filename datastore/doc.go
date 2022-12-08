907c27ee93090c1bc9fddcd9176ff36459a25037
disexi <159637451+disexi@users.noreply.github.com>
2022-12-08 21:34:13 +0000

    footer updated

--- datastore/doc.go
+++ datastore/doc.go
@@ -1,2 +1,11 @@
 /*
-Datastore package define several interfaces for gchain
+Datastore package define several interfaces for gchain to interact with data structure.
+
+ 1. Datastore
+    It define a general interface to any database system available and use it to store and search for data.
+ 2. Vectorstore
+    Fully compatible with datastore interface with addition of SearchVector functionality. Vector search database will prefer to use this interface.
+ 3. Retriever
+    Provide simplified interface to get data from outside world, intended to interact in read only manner.
+
+Datastore and V
