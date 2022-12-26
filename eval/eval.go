6a64fa31424f457da12f9c9c18dcb90f993d1a02
disexi <159637451+disexi@users.noreply.github.com>
2022-12-26 20:17:47 +0000

    Fix linux build.

--- eval/eval.go
+++ eval/eval.go
@@ -1,3 +1,16 @@
 /*
 Package eval provide evaluator for gchain
-especially t
+especially to evaluate LLM output using LLM output
+
+Use this package for integration test or evaluating llm output
+*/
+package eval
+
+type Evaluator interface {
+	Evaluate(input string) (bool, error)
+}
+
+type llmEvalOutput struct {
+	Pass   bool   `json:"pass"`
+	Reason string `json:"reason"`
+}
---
+++ eval/utility_eval.go
@@ -0,0 +1,7 @@
+package eval
+
+import "encoding/json"
+
+// ValidJson will check if input is valid json
+type ValidJson struct {
+}
