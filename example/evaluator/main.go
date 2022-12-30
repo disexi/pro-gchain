6158516320f07b31b318511dd09b2a9d744eb3e8
disexi <159637451+disexi@users.noreply.github.com>
2022-12-30 14:43:22 +0000

    VC4.0.2

--- example/evaluator/main.go
+++ example/evaluator/main.go
@@ -7,4 +7,30 @@ accept command flag -o to capture string
 
 import (
 	"encoding/csv"
-	"f
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/wejick/gchain/callback"
+	"github.com/wejick/gchain/eval"
+	"github.com/wejick/gchain/model"
+	_openai "github.com/wejick/gchain/model/openAI"
+)
+
+var (
+	o string
+)
+
+func init() {
+	flag.StringVar(&o, "o", "", "output file")
+	flag.Parse()
+}
+
+type Test struct {
+	Name        string
+	Evaluator   string
+	Input       string
+	Expectation string
+	Reason      string
+	Result      b
