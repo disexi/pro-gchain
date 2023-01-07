47d73d6e8a107183bdfa35a39d673ca64ed314e4
disexi <159637451+disexi@users.noreply.github.com>
2023-01-07 12:09:29 +0000

    updated resume

--- example/functioncall/main.go
+++ example/functioncall/main.go
@@ -6,4 +6,18 @@ import (
 	"log"
 	"os"
 
-	
+	"github.com/wejick/gchain/callback"
+	"github.com/wejick/gchain/model"
+	_openai "github.com/wejick/gchain/model/openAI"
+	"github.com/wejick/gchain/tools/greeting"
+)
+
+func main() {
+	var authToken = os.Getenv("OPENAI_API_KEY")
+	chatModel := _openai.NewOpenAIChatModel(authToken, _openai.GPT3Dot5Turbo0301, callback.NewManager())
+	memory := []model.ChatMessage{}
+
+	greeter := greeting.NewGreetingTool()
+
+	// prepare a function register
+	functionList := map[string
