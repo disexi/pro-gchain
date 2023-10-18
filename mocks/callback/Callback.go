cdcbf247ddfb32ae6e677e359b755ace743b042d
disexi <159637451+disexi@users.noreply.github.com>
2023-10-18 18:07:42 +0000

    Regex fix

--- mocks/callback/Callback.go
+++ mocks/callback/Callback.go
@@ -20,4 +20,16 @@ func (_m *Callback) Execute(_a0 context.Context, data callback.CallbackData) {
 	_m.Called(_a0, data)
 }
 
-// NewCallback creates a new instance of Callback. It also registers a testing interface on the moc
+// NewCallback creates a new instance of Callback. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
+// The first argument is typically a *testing.T value.
+func NewCallback(t interface {
+	mock.TestingT
+	Cleanup(func())
+}) *Callback {
+	mock := &Callback{}
+	mock.Mock.Test(t)
+
+	t.Cleanup(func() { mock.AssertExpectations(t) })
+
+	return mock
+}
