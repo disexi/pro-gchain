a84d1dffa3f3a90deafc0a0fef5e1dc7d975c421
disexi <159637451+disexi@users.noreply.github.com>
2023-11-03 15:19:21 +0000

    removed submodules

--- mocks/model/ChatModel.go
+++ mocks/model/ChatModel.go
@@ -17,3 +17,18 @@ type ChatModel struct {
 // Call provides a mock function with given fields: ctx, prompt, options
 func (_m *ChatModel) Call(ctx context.Context, prompt string, options ...func(*model.Option)) (string, error) {
 	_va := make([]interface{}, len(options))
+	for _i := range options {
+		_va[_i] = options[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, prompt)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 string
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, ...func(*model.Option)) (string, error)); ok {
+		return rf(ctx, prompt, options...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, string, ...func(*model.Option)) string); ok {
+		r0 = rf(ctx, prompt, options...
