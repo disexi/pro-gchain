911a3b2d01a143014393944df514571b21ff2e95
disexi <159637451+disexi@users.noreply.github.com>
2023-11-16 08:48:37 +0000

    Updated Readme.md.

--- model/chatmessage.go
+++ model/chatmessage.go
@@ -1,4 +1,22 @@
 package model
 
 type ChatMessage struct {
-	
+	Role          string
+	Content       string
+	Name          string `json:"name"`
+	ParameterJson string // json string of function parameter
+	PromptUsage   PromptUsage  `json:"usage"`
+}
+
+type PromptUsage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
+func (C *ChatMessage) String() string {
+	return C.Role + ": " + C.Content
+}
+
+const (
+	ChatMessageRole
