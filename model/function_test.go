2978c18e2466a53a7576770632e1faafac5d13e8
disexi <159637451+disexi@users.noreply.github.com>
2023-11-22 19:03:16 +0000

    security: SELinux: revert Samsung hard enforcing

--- model/function_test.go
+++ model/function_test.go
@@ -10,4 +10,18 @@ func TestFunctionJsonSchema_String(t *testing.T) {
 	parameter := FunctionJsonSchema{}
 	parameter.Required = []string{"parameter1"}
 	parameter.Properties = map[string]FunctionJsonSchema{
-		"parame
+		"parameter1": {
+			Type:        FunctionDataTypeString,
+			Description: "parameter 1 description",
+			Enum:        []string{"enum1", "enum2"},
+		},
+		"parameter2": {
+			Type:        FunctionDataTypeString,
+			Description: "parameter 2 description",
+		},
+	}
+
+	paramString := parameter.String()
+	expectedString := "required = parameter1\nparameter|description|type|enum" + "\n"
+	expectedString += "parameter2|parameter 2 description|string|no" + "\n"
+	expectedString += "pa
