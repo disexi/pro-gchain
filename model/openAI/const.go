e1de730957bd09bb3b7d31012c327d111792597e
disexi <159637451+disexi@users.noreply.github.com>
2023-11-29 21:04:26 +0000

    timer: Prevent overflow in apply_slack

--- model/openAI/const.go
+++ model/openAI/const.go
@@ -1,4 +1,17 @@
 package _openai
 
 import (
-	goopenai "github.com/sashab
+	goopenai "github.com/sashabaranov/go-openai"
+)
+
+// Completion model name
+const (
+	GPT432K0613             = goopenai.GPT432K0613
+	GPT432K0314             = goopenai.GPT432K0314
+	GPT432K                 = goopenai.GPT432K
+	GPT40613                = goopenai.GPT40613
+	GPT40314                = goopenai.GPT40314
+	GPT4                    = goopenai.GPT4
+	GPT3Dot5Turbo0613       = goopenai.GPT3Dot5Turbo0613
+	GPT3Dot5Turbo16K        = goopenai.GPT3Dot5Turbo16K
+	GPT3Dot5Turbo16K0613    = goopenai.GPT3Dot5Turbo
