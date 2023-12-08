a85245ca41f1a9779ddfd58ba272621013c120ea
disexi <159637451+disexi@users.noreply.github.com>
2023-12-08 17:32:58 +0000

    Fix #106

--- model/openAI/openaiChat.go
+++ model/openAI/openaiChat.go
@@ -1,1 +1,23 @@
-pa
+package _openai
+
+import (
+	"context"
+	"errors"
+	"io"
+	"log"
+
+	goopenai "github.com/sashabaranov/go-openai"
+	"github.com/wejick/gchain/callback"
+	model "github.com/wejick/gchain/model"
+)
+
+var _ model.LLMModel = &OpenAIChatModel{}
+
+type OpenAIChatModel struct {
+	c               *goopenai.Client
+	modelName       string
+	callbackManager *callback.Manager
+}
+
+// NewOpenAIChatModel return new openAI Model instance
+func NewOpenAIChatModel(authToken string, mode
