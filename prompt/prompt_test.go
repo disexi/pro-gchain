ec8ff88be227671a493db6ce4bc8d3c0e0d0ba2c
disexi <159637451+disexi@users.noreply.github.com>
2024-02-23 19:48:28 +0000

    Optimise images

--- prompt/prompt_test.go
+++ prompt/prompt_test.go
@@ -6,4 +6,23 @@ import (
 
 func TestPrompt_template_FormatPrompt(t *testing.T) {
 	type args struct {
-		Data map[stri
+		Data map[string]string
+	}
+	tests := []struct {
+		name              string
+		templateName      string
+		templateString    string
+		promptTemplate    *PromptTemplate
+		args              args
+		wantOutput_prompt string
+		wantErr           bool
+	}{
+		{
+			name:           "empty",
+			templateName:   "empty",
+			templateString: "",
+			wantErr:        false,
+		},
+		{
+			name:           "{{.string}} {{.stringfloat}} {{.stringinteger}}",
+			templateName:   "
