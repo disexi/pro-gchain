98eac4ebb993dd2d1a4b4b2eca9373f60abe456c
disexi <159637451+disexi@users.noreply.github.com>
2024-02-23 13:33:48 +0000

    forgot to build

--- prompt/doc.go
+++ prompt/doc.go
@@ -1,2 +1,12 @@
 /*
-	A “prompt” refers to the inp
+	A “prompt” refers to the input to the model.
+
+This input is rarely hard coded, but rather is often constructed from multiple components.
+A PromptTemplate is responsible for the construction of this input.
+
+PromptTemplate is powered by Go text/template, however we provided simplistic interface to interact with.
+Everything is a string and data passed along to the template as map[string]string.
+
+Example :
+
+	template, _ := NewPromptT
