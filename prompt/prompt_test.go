e1e41fb9ee55b17536d4fa82daea3c975b9edf83
disexi <159637451+disexi@users.noreply.github.com>
2024-03-13 16:25:56 +0000

    add sponsors

--- prompt/prompt_test.go
+++ prompt/prompt_test.go
@@ -25,4 +25,17 @@ func TestPrompt_template_FormatPrompt(t *testing.T) {
 		},
 		{
 			name:           "{{.string}} {{.stringfloat}} {{.stringinteger}}",
-			templateName:   "
+			templateName:   "tplt",
+			templateString: "{{.string}} {{.stringfloat}} {{.stringinteger}}",
+			args: args{
+				Data: map[string]string{"string": "string", "stringfloat": "0.1", "stringinteger": "1"},
+			},
+			wantErr:           false,
+			wantOutput_prompt: "string 0.1 1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			P, err := NewPromptTemplate(tt.templateName, tt.templateString)
+			if err != nil {
+				t.Error
