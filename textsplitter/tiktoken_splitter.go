e8f965caf213e2b73fc94b52d8ce1d9844835b70
disexi <159637451+disexi@users.noreply.github.com>
2024-03-14 06:21:25 +0000

    Added a todo.

--- textsplitter/tiktoken_splitter.go
+++ textsplitter/tiktoken_splitter.go
@@ -13,4 +13,22 @@ type TikTokenSplitter struct {
 
 // NewTikTokenSplitter create new TikTokenSplitter instance
 // if modelName empty, the default one is gpt-3.5-turbo-0301
-func NewTikTokenSplit
+func NewTikTokenSplitter(modelName string) (*TikTokenSplitter, error) {
+	if modelName == "" {
+		modelName = "gpt-3.5-turbo-0301"
+	}
+
+	tkm, err := tiktoken.EncodingForModel(modelName)
+	return &TikTokenSplitter{
+		tkm: tkm,
+	}, err
+}
+
+// SplitText creates chunks where length's doesn't exceed maxChunkSize.
+func (T *TikTokenSplitter) SplitText(input string, maxChunkSize int, overlap int) []string {
+	if input == "" {
+		return []string{}
+	}
+	batches := []string{}
+
+	words := strings.Fields(inpu
