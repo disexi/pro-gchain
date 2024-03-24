29e41f4b5c2a42a82bb0e8268c161398fc9a970c
disexi <159637451+disexi@users.noreply.github.com>
2024-03-24 07:09:18 +0000

    remove gitignore

--- textsplitter/word_splitter.go
+++ textsplitter/word_splitter.go
@@ -10,4 +10,23 @@ type WordSplitter struct {
 }
 
 // splitIntoBatches creates word batches where length's doesn't exceed maxChunkSize.
-func (W *WordSplitter) Sp
+func (W *WordSplitter) SplitText(input string, maxChunkSize int, overlap int) []string {
+	batches := []string{}
+
+	words := strings.Fields(input)
+	var batch []string
+	var lenCounter int
+
+	for _, word := range words {
+		// +1 is for a possible space character
+		if lenCounter+len(word)+1 > maxChunkSize {
+			batches = append(batches, strings.Join(batch, " "))
+			batch = []string{}
+			lenCounter = 0
+		}
+
+		batch = append(batch, word)
+		lenCounter += len(word) + 1
+	}
+
+	if len(batch)
