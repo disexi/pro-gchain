41837053b5c2eeec6f12eab8701c04bb5568f96b
disexi <159637451+disexi@users.noreply.github.com>
2024-03-26 10:20:31 +0000

    Updating website

--- tools/greeting/greeting.go
+++ tools/greeting/greeting.go
@@ -12,3 +12,15 @@ import (
 type GreetingTool struct {
 	functionDefinition model.FunctionDefinition
 }
+
+func NewGreetingTool() *GreetingTool {
+	return &GreetingTool{
+		functionDefinition: model.FunctionDefinition{
+			Name:        "greeting_tool",
+			Description: "This tool is used to greet user with hello",
+			Parameters: model.FunctionJsonSchema{
+				Type: model.FunctionDataTypeObject,
+				Properties: map[string]model.FunctionJsonSchema{
+					"user_name": {
+						Type:        model.FunctionDataTypeString,
+						Description: "User name"
