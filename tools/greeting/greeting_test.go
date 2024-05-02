6fe74d54101249e419be598b5e38b9f887629e2c
disexi <159637451+disexi@users.noreply.github.com>
2024-05-02 16:40:18 +0000

    help message

--- tools/greeting/greeting_test.go
+++ tools/greeting/greeting_test.go
@@ -14,4 +14,16 @@ func TestGreetingsTool(t *testing.T) {
 	assert.NotNil(t, greetingTool.GetFunctionDefinition())
 
 	// test simple run
-	output, err := greetingTool.SimpleRun(context
+	output, err := greetingTool.SimpleRun(context.Background(), `{"user_name":"John"}`)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello John welcome to the paradise of the world", output)
+
+	// test simple run with invalid input
+	output, err = greetingTool.SimpleRun(context.Background(), `123`)
+	assert.NotNil(t, err)
+	assert.Equal(t, "", output)
+
+	// test run
+	outputRun, err := greetingTool.Run(context.Background(), map[string]string{"user_name": "John"})
+	assert.Nil(t, err)
+	assert.Equal(t, map[st
